c98588af04df0c22b7ec1230de7e9507c1679029
eugene <eugenebokhan@icloud.com>
2025-11-03 13:00:03 +0000

    track dlpack

---
+++ external/xgrammar-0.1.26/3rdparty/dlpack/tests/scripts/task_lint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+mkdir -p bin
+
+set -e -o pipefail
+
+if [ ! -f bin/lint.py ]; then
+    echo "Grab linter ..."
+    wget https://raw.githubusercontent.com/dmlc/dmlc-core/28c2bfd2a8cf46e632e30d7fec006d3ee4b26b1c/scripts/lint.py
+    mv lint.py bin/lint.py
+fi
+
+echo "Check codestyle of c++ code..."
+python bin/lint.py dlpack cpp include contrib
+
+set +e
+
+echo "Check doxygen generation..."
+make doc 2>log.txt
+(cat log.txt| grep -v ENABLE_PREPROCESSING |grep -v "unsupported tag") > logclean.txt
+echo "---------Error Log----------"
+cat logclean.txt
+echo "----------------------------"
+(cat logclean.txt|grep warning) && exit -1
+(cat logclean.txt|grep error) && exit -1
+rm logclean.txt
+rm log.txt
+echo "All checks passed..."
